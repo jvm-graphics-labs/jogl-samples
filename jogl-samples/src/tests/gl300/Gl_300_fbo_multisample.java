4ebba22a4da1ea2aef637b3c6674d9b4dfea673f
Giuseppe Barbieri <elect86@gmail.com>
2015-10-16 17:05:22 +0200

    gl-300-fbo-multisample in progress

--- jogl-samples/src/tests/gl300/Gl_300_fbo_multisample.java
+++ jogl-samples/src/tests/gl300/Gl_300_fbo_multisample.java
@@ -8,18 +8,34 @@ package tests.gl300;
 import com.jogamp.common.util.IOUtil;
 import com.jogamp.opengl.GL;
 import static com.jogamp.opengl.GL.GL_ARRAY_BUFFER;
+import static com.jogamp.opengl.GL.GL_COLOR_ATTACHMENT0;
+import static com.jogamp.opengl.GL.GL_COLOR_BUFFER_BIT;
+import static com.jogamp.opengl.GL.GL_DRAW_FRAMEBUFFER;
+import static com.jogamp.opengl.GL.GL_FLOAT;
+import static com.jogamp.opengl.GL.GL_FRAMEBUFFER;
+import static com.jogamp.opengl.GL.GL_FRAMEBUFFER_COMPLETE;
 import static com.jogamp.opengl.GL.GL_LINEAR;
 import static com.jogamp.opengl.GL.GL_LINEAR_MIPMAP_LINEAR;
+import static com.jogamp.opengl.GL.GL_MULTISAMPLE;
+import static com.jogamp.opengl.GL.GL_NEAREST;
+import static com.jogamp.opengl.GL.GL_READ_FRAMEBUFFER;
+import static com.jogamp.opengl.GL.GL_RENDERBUFFER;
+import static com.jogamp.opengl.GL.GL_RGBA;
+import static com.jogamp.opengl.GL.GL_RGBA8;
 import static com.jogamp.opengl.GL.GL_STATIC_DRAW;
 import static com.jogamp.opengl.GL.GL_TEXTURE0;
 import static com.jogamp.opengl.GL.GL_TEXTURE_2D;
 import static com.jogamp.opengl.GL.GL_TEXTURE_MAG_FILTER;
 import static com.jogamp.opengl.GL.GL_TEXTURE_MIN_FILTER;
+import static com.jogamp.opengl.GL.GL_TRIANGLES;
+import static com.jogamp.opengl.GL.GL_UNSIGNED_BYTE;
 import static com.jogamp.opengl.GL2ES2.GL_FRAGMENT_SHADER;
 import static com.jogamp.opengl.GL2ES2.GL_VERTEX_SHADER;
+import static com.jogamp.opengl.GL2ES3.GL_COLOR;
 import static com.jogamp.opengl.GL2ES3.GL_TEXTURE_BASE_LEVEL;
 import static com.jogamp.opengl.GL2ES3.GL_TEXTURE_MAX_LEVEL;
 import com.jogamp.opengl.GL3;
+import com.jogamp.opengl.math.FloatUtil;
 import com.jogamp.opengl.util.GLBuffers;
 import com.jogamp.opengl.util.glsl.ShaderCode;
 import com.jogamp.opengl.util.glsl.ShaderProgram;
@@ -31,6 +47,8 @@ import java.net.URLConnection;
 import java.nio.FloatBuffer;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import jgli.Gl;
+import jgli.Load;
 import jglm.Vec2i;
 //import texture.TextureData;
 //import texture.TextureIO;
@@ -46,8 +64,13 @@ public class Gl_300_fbo_multisample extends Test {
         Gl_300_fbo_multisample gl_300_fbo_multisample = new Gl_300_fbo_multisample();
     }
 
+    public Gl_300_fbo_multisample() {
+        super("gl-300-fbo-multisample", 3, 0);
+    }
+
     private final String SHADERS_SOURCE = "image-2d";
     private final String SHADERS_ROOT = "src/data/gl_300";
+    private final String TEXTURE_ROOT = "/data";
     private final String TEXTURE_DIFFUSE = "kueken7_rgba8_srgb.dds";
     private Vec2i FRAMEBUFFER_SIZE = new Vec2i(160, 120);
     // With DDS textures, v texture coordinate are reversed, from top to bottom
@@ -55,30 +78,16 @@ public class Gl_300_fbo_multisample extends Test {
     private int vertexSize = vertexCount * 4 * GLBuffers.SIZEOF_FLOAT;
     private float[] vertexData = new float[]{
         -2.0f, -1.5f, 0.0f, 0.0f,
-        2.0f, -1.5f, 1.0f, 0.0f,
-        2.0f, 1.5f, 1.0f, 1.0f,
-        2.0f, 1.5f, 1.0f, 1.0f,
-        -2.0f, 1.5f, 0.0f, 1.0f,
+        +2.0f, -1.5f, 1.0f, 0.0f,
+        +2.0f, +1.5f, 1.0f, 1.0f,
+        +2.0f, +1.5f, 1.0f, 1.0f,
+        -2.0f, +1.5f, 0.0f, 1.0f,
         -2.0f, -1.5f, 0.0f, 0.0f
     };
     private int programName, uniformMvp, uniformDiffuse;
-    private int[] vertexArrayName, bufferName, textureName, colorRenderbufferName, colorTextureName,
-            framebufferRenderName, framebufferResolveName;
-
-    public Gl_300_fbo_multisample() {
-
-        super("Gl_300_fbo_multisample", 3, 0);
-        programName = 0;
-        vertexArrayName = new int[]{0};
-        bufferName = new int[]{0};
-        textureName = new int[]{0};
-        colorRenderbufferName = new int[]{0};
-        colorTextureName = new int[]{0};
-        framebufferRenderName = new int[]{0};
-        framebufferResolveName = new int[]{0};
-        uniformMvp = -1;
-        uniformDiffuse = -1;
-    }
+    private int[] vertexArrayName, bufferName, textureName, colorRenderbufferName,
+            colorTextureName, framebufferRenderName, framebufferResolveName;
+    private float[] perspective = new float[16], model = new float[16], mvp = new float[16];
 
     @Override
     protected boolean begin(GL gl) {
@@ -88,10 +97,19 @@ public class Gl_300_fbo_multisample extends Test {
         boolean validated = true;
 
         if (validated) {
-            initProgram(gl3);
+            validated = initProgram(gl3);
         }
         if (validated) {
-            initTexture(gl3);
+            validated = initBuffer(gl3);
+        }
+        if (validated) {
+            validated = initVertexArray(gl3);
+        }
+        if (validated) {
+            validated = initTexture(gl3);
+        }
+        if (validated) {
+            validated = initFramebuffer(gl3);
         }
 
         return validated & checkError(gl, "begin");
@@ -115,13 +133,13 @@ public class Gl_300_fbo_multisample extends Test {
 
             programName = program.program();
 
-            gl3.glBindAttribLocation(programName, Semantic.Attr.POSITION, "position");
-            gl3.glBindAttribLocation(programName, Semantic.Attr.TEXCOORD, "texCoord");
+            gl3.glBindAttribLocation(programName, Semantic.Attr.POSITION, "Position");
+            gl3.glBindAttribLocation(programName, Semantic.Attr.TEXCOORD, "TexCoord");
         }
         if (validated) {
 
-            uniformMvp = gl3.glGetUniformLocation(programName, "mvp");
-            uniformDiffuse = gl3.glGetUniformLocation(programName, "diffuse");
+            uniformMvp = gl3.glGetUniformLocation(programName, "MVP");
+            uniformDiffuse = gl3.glGetUniformLocation(programName, "Diffuse");
         }
         return validated & checkError(gl3, "initProgram");
     }
@@ -140,40 +158,162 @@ public class Gl_300_fbo_multisample extends Test {
 
     private boolean initTexture(GL3 gl3) {
 
-//        try {
-////            jgli.Texture
-//
-//            gl3.glGenTextures(1, textureName, 0);
-//            gl3.glActiveTexture(GL_TEXTURE0);
-//            gl3.glBindTexture(GL_TEXTURE_2D, textureName[0]);
-//            gl3.glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_BASE_LEVEL, 0);
-//            gl3.glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAX_LEVEL, ddsImage.getNumMipMaps() - 1);
-//            gl3.glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
-//            gl3.glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-//
-//            System.out.println("" + ddsImage.isCompressed() + " " + ddsImage.getNumMipMaps());
-//
-//            TextureData textureData = TextureIO.newTextureData(gl3.getGLProfile(),
-//                    conn.getURL(), ddsImage.getNumMipMaps() > 1, TextureIO.DDS);
-//
-//            for (int level = 0; level < ddsImage.getNumMipMaps(); level++) {
-//
-////                ImageInfo imageInfo = ddsImage.getMipMap(level);
-//////                System.out.println("imageInfo.toString() " + ddsImage.getCompressionFormat());
-////
-//////                TextureData textureData = new TextureData(gl3.getGLProfile(),
-//////                        ddsImage.getCompressionFormat(), ddsImage.getWidth(), ddsImage.getHeight(),
-//////                        0, ddsImage.getPixelFormat(), GL_UNSIGNED_BYTE, false, true, false,
-//////                        imageInfo.getData(), null);
-//////                System.out.println("data.toString() " + textureData.toString());
-////                gl3.glTexImage2D(GL_TEXTURE_2D, level, textureData.getInternalFormat(),
-////                        imageInfo.getWidth(), imageInfo.getHeight(), 0, textureData.getPixelFormat(), 
-////                        textureData.getPixelType(), textureData.getBuffer());
-//            }
-//        } catch (IOException ex) {
-//            Logger.getLogger(Gl_300_fbo_multisample.class.getName()).log(Level.SEVERE, null, ex);
-//        }
-        return false;
+        try {
+            jgli.Texture texture = Load.load(TEXTURE_ROOT + "/" + TEXTURE_DIFFUSE);
+
+            textureName = new int[1];
+            gl3.glGenTextures(1, textureName, 0);
+            gl3.glActiveTexture(GL_TEXTURE0);
+            gl3.glBindTexture(GL_TEXTURE_2D, textureName[0]);
+            gl3.glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_BASE_LEVEL, 0);
+            gl3.glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAX_LEVEL, texture.levels() - 1);
+            gl3.glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+            gl3.glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+            jgli.Gl.Format glFormat = Gl.instance.translate(texture.format);
+
+            for (int level = 0; level < texture.levels(); level++) {
+
+                gl3.glTexImage2D(GL_TEXTURE_2D, level, glFormat.internal.value,
+                        texture.dimensions(level)[0], texture.dimensions(level)[1], 0,
+                        glFormat.external.value, glFormat.type.value, texture.data(0, 0, level));
+            }
+        } catch (IOException ex) {
+            Logger.getLogger(Gl_300_fbo_multisample.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return checkError(gl3, "initTexture");
     }
 
+    private boolean initFramebuffer(GL3 gl3) {
+
+        colorRenderbufferName = new int[1];
+        gl3.glGenRenderbuffers(1, colorRenderbufferName, 0);
+        gl3.glBindRenderbuffer(GL_RENDERBUFFER, colorRenderbufferName[0]);
+        gl3.glRenderbufferStorageMultisample(GL_RENDERBUFFER, 8, GL_RGBA8, FRAMEBUFFER_SIZE.x, FRAMEBUFFER_SIZE.y);
+        // The second parameter is the number of samples.
+
+        framebufferRenderName = new int[1];
+        gl3.glGenFramebuffers(1, framebufferRenderName, 0);
+        gl3.glBindFramebuffer(GL_FRAMEBUFFER, framebufferRenderName[0]);
+        gl3.glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_RENDERBUFFER, colorRenderbufferName[0]);
+        if (gl3.glCheckFramebufferStatus(GL_FRAMEBUFFER) != GL_FRAMEBUFFER_COMPLETE) {
+            return false;
+        }
+        gl3.glBindFramebuffer(GL_FRAMEBUFFER, 0);
+
+        colorTextureName = new int[1];
+        gl3.glGenTextures(1, colorTextureName, 0);
+        gl3.glBindTexture(GL_TEXTURE_2D, colorTextureName[0]);
+        gl3.glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+        gl3.glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+        gl3.glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA8, FRAMEBUFFER_SIZE.x, FRAMEBUFFER_SIZE.y,
+                0, GL_RGBA, GL_UNSIGNED_BYTE, 0);
+
+        framebufferResolveName = new int[1];
+        gl3.glGenFramebuffers(1, framebufferResolveName, 0);
+        gl3.glBindFramebuffer(GL_FRAMEBUFFER, framebufferResolveName[0]);
+        gl3.glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, colorTextureName[0], 0);
+        if (gl3.glCheckFramebufferStatus(GL_FRAMEBUFFER) != GL_FRAMEBUFFER_COMPLETE) {
+            return false;
+        }
+        gl3.glBindFramebuffer(GL_FRAMEBUFFER, 0);
+
+        return checkError(gl3, "initFramebuffer");
+    }
+
+    private boolean initVertexArray(GL3 gl3) {
+
+        vertexArrayName = new int[1];
+        gl3.glGenVertexArrays(1, vertexArrayName, 0);
+        gl3.glBindVertexArray(vertexArrayName[0]);
+        gl3.glBindBuffer(GL_ARRAY_BUFFER, bufferName[0]);
+        gl3.glVertexAttribPointer(Semantic.Attr.POSITION, 2, GL_FLOAT, false, 2 * 2 * Float.BYTES, 0);
+        gl3.glVertexAttribPointer(Semantic.Attr.TEXCOORD, 2, GL_FLOAT, false, 2 * 2 * Float.BYTES, 2 * Float.BYTES);
+        gl3.glBindBuffer(GL_ARRAY_BUFFER, 0);
+
+        gl3.glEnableVertexAttribArray(Semantic.Attr.POSITION);
+        gl3.glEnableVertexAttribArray(Semantic.Attr.TEXCOORD);
+        gl3.glBindVertexArray(0);
+
+        return checkError(gl3, "initVertexArray");
+    }
+
+    @Override
+    public boolean render(GL gl) {
+
+        GL3 gl3 = (GL3) gl;
+
+        // Clear the framebuffer
+        gl3.glBindFramebuffer(GL_FRAMEBUFFER, 0);
+        gl3.glClearBufferfv(GL_COLOR, 0, new float[]{1.0f, 0.5f, 0.0f, 1.0f}, 0);
+
+        gl3.glUseProgram(programName);
+        gl3.glUniform1i(uniformDiffuse, 0);
+
+		// Pass 1
+        // Render the scene in a multisampled framebuffer
+        gl3.glEnable(GL_MULTISAMPLE);
+        renderFBO(gl3, framebufferRenderName[0]);
+        gl3.glDisable(GL_MULTISAMPLE);
+
+        // Resolved multisampling
+        gl3.glBindFramebuffer(GL_READ_FRAMEBUFFER, framebufferRenderName[0]);
+        gl3.glBindFramebuffer(GL_DRAW_FRAMEBUFFER, framebufferResolveName[0]);
+        gl3.glBlitFramebuffer(
+                0, 0, FRAMEBUFFER_SIZE.x, FRAMEBUFFER_SIZE.y,
+                0, 0, FRAMEBUFFER_SIZE.x, FRAMEBUFFER_SIZE.y,
+                GL_COLOR_BUFFER_BIT, GL_LINEAR);
+        gl3.glBindFramebuffer(GL_FRAMEBUFFER, 0);
+
+		// Pass 2
+        // Render the colorbuffer from the multisampled framebuffer
+        gl3.glViewport(0, 0, windowSize.x, windowSize.y);
+        renderFB(gl3, colorTextureName[0]);
+
+        return true;
+    }
+
+    private void renderFB(GL3 gl3, int texture2dName) {
+
+        FloatUtil.makePerspective(perspective, 0, true, (float) (Math.PI * 0.25f),
+                (float) windowSize.x / (float) windowSize.y, 0.1f, 100.0f);
+        FloatUtil.makeIdentity(model);
+        FloatUtil.multMatrix(perspective, view(), mvp);
+        FloatUtil.multMatrix(mvp, model);
+        gl3.glUniformMatrix4fv(uniformMvp, 1, false, mvp, 0);
+
+        gl3.glActiveTexture(GL_TEXTURE0);
+        gl3.glBindTexture(GL_TEXTURE_2D, texture2dName);
+
+        gl3.glBindVertexArray(vertexArrayName[0]);
+        gl3.glDrawArrays(GL_TRIANGLES, 0, vertexCount);
+
+        checkError(gl3, "renderFB");
+    }
+
+    private void renderFBO(GL3 gl3, int Framebuffer) {
+
+        gl3.glBindFramebuffer(GL_FRAMEBUFFER, Framebuffer);
+        gl3.glClearColor(0.0f, 0.5f, 1.0f, 1.0f);
+        gl3.glClear(GL_COLOR_BUFFER_BIT);
+
+        FloatUtil.makePerspective(perspective, 0, true, (float) (Math.PI * 0.25f),
+                (float) (FRAMEBUFFER_SIZE.x) / FRAMEBUFFER_SIZE.y, 0.1f, 100.0f);
+        FloatUtil.makeIdentity(model);
+        FloatUtil.multMatrix(perspective, view(), mvp);
+        FloatUtil.multMatrix(mvp, model);
+        gl3.glUniformMatrix4fv(uniformMvp, 1, false, mvp, 0);
+
+        gl3.glViewport(0, 0, FRAMEBUFFER_SIZE.x, FRAMEBUFFER_SIZE.y);
+        gl3.glClearColor(0.0f, 0.5f, 1.0f, 1.0f);
+        gl3.glClear(GL_COLOR_BUFFER_BIT);
+
+        gl3.glActiveTexture(GL_TEXTURE0);
+        gl3.glBindTexture(GL_TEXTURE_2D, textureName[0]);
+
+        gl3.glBindVertexArray(vertexArrayName[0]);
+        gl3.glDrawArrays(GL_TRIANGLES, 0, vertexCount);
+
+        checkError(gl3, "renderFBO");
+    }
 }
