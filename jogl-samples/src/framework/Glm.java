0328879b85d534cd34e996eba7835eca4a4d2a4d
Giuseppe Barbieri <elect86@gmail.com>
2015-10-27 17:11:02 +0100

    320-fbo-depth-stencil

---
+++ jogl-samples/src/framework/Glm.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package framework;
+
+/**
+ *
+ * @author GBarbieri
+ */
+public class Glm {
+
+    public static int ceilMultiple(int source, int multiple) {
+        return source + (multiple - (source % multiple));
+    }
+}
