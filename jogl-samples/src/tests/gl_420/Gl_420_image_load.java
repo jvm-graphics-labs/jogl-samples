83a309daa6c8128bcfe36f3e087816415e054736
Giuseppe Barbieri <elect86@gmail.com>
2016-01-12 13:24:01 +0100

    gotcha

--- jogl-samples/src/tests/gl_420/Gl_420_image_load.java
+++ jogl-samples/src/tests/gl_420/Gl_420_image_load.java
@@ -311,8 +311,6 @@ public class Gl_420_image_load extends Test {
 
 //            pointer.putFloat(imageSize.x);
 //            pointer.putFloat(imageSize.y);
-//            pointer.putFloat((float)((long)imageSize.x));
-//            pointer.putFloat((float)((long)imageSize.y));
             pointer.put((byte) (0 & 0xff));
             pointer.put((byte) (1 & 0xff));
             pointer.put((byte) (0 & 0xff));
