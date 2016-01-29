1d6f249759383ef7ca36c76f57f389b9447ce022
Giuseppe Barbieri <elect86@gmail.com>
2016-01-29 09:46:37 +0100

    gl-430-texture-copy

---
+++ jogl-samples/src/tests/gl_430/Gl_430_texture_copy.java
@@ -0,0 +1,317 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tests.gl_430;
+
+import com.jogamp.opengl.GL;
+import static com.jogamp.opengl.GL.GL_ALPHA;
+import static com.jogamp.opengl.GL.GL_ARRAY_BUFFER;
+import static com.jogamp.opengl.GL.GL_DYNAMIC_DRAW;
+import static com.jogamp.opengl.GL.GL_FLOAT;
+import static com.jogamp.opengl.GL.GL_MAP_INVALIDATE_BUFFER_BIT;
+import static com.jogamp.opengl.GL.GL_MAP_WRITE_BIT;
+import static com.jogamp.opengl.GL.GL_NEAREST;
+import static com.jogamp.opengl.GL.GL_STATIC_DRAW;
+import static com.jogamp.opengl.GL.GL_TEXTURE0;
+import static com.jogamp.opengl.GL.GL_TEXTURE_2D;
+import static com.jogamp.opengl.GL.GL_TEXTURE_MAG_FILTER;
+import static com.jogamp.opengl.GL.GL_TEXTURE_MIN_FILTER;
+import static com.jogamp.opengl.GL.GL_TRIANGLES;
+import static com.jogamp.opengl.GL.GL_TRUE;
+import static com.jogamp.opengl.GL.GL_UNPACK_ALIGNMENT;
+import static com.jogamp.opengl.GL2ES2.GL_FRAGMENT_SHADER;
+import static com.jogamp.opengl.GL2ES2.GL_FRAGMENT_SHADER_BIT;
+import static com.jogamp.opengl.GL2ES2.GL_PROGRAM_SEPARABLE;
+import static com.jogamp.opengl.GL2ES2.GL_RED;
+import static com.jogamp.opengl.GL2ES2.GL_VERTEX_SHADER;
+import static com.jogamp.opengl.GL2ES2.GL_VERTEX_SHADER_BIT;
+import static com.jogamp.opengl.GL2ES3.GL_BLUE;
+import static com.jogamp.opengl.GL2ES3.GL_COLOR;
+import static com.jogamp.opengl.GL2ES3.GL_GREEN;
+import static com.jogamp.opengl.GL2ES3.GL_TEXTURE_BASE_LEVEL;
+import static com.jogamp.opengl.GL2ES3.GL_TEXTURE_MAX_LEVEL;
+import static com.jogamp.opengl.GL2ES3.GL_TEXTURE_SWIZZLE_A;
+import static com.jogamp.opengl.GL2ES3.GL_TEXTURE_SWIZZLE_B;
+import static com.jogamp.opengl.GL2ES3.GL_TEXTURE_SWIZZLE_G;
+import static com.jogamp.opengl.GL2ES3.GL_TEXTURE_SWIZZLE_R;
+import static com.jogamp.opengl.GL2ES3.GL_UNIFORM_BUFFER;
+import com.jogamp.opengl.GL4;
+import com.jogamp.opengl.util.GLBuffers;
+import com.jogamp.opengl.util.glsl.ShaderCode;
+import com.jogamp.opengl.util.glsl.ShaderProgram;
+import core.glm;
+import dev.Mat4;
+import dev.Vec2;
+import framework.Profile;
+import framework.Semantic;
+import framework.Test;
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.FloatBuffer;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import jgli.Texture2d;
+
+/**
+ *
+ * @author GBarbieri
+ */
+public class Gl_430_texture_copy extends Test {
+
+    public static void main(String[] args) {
+        Gl_430_texture_copy gl_430_texture_copy = new Gl_430_texture_copy();
+    }
+
+    public Gl_430_texture_copy() {
+        super("gl-430-texture-copy", Profile.CORE, 4, 3);
+    }
+
+    private final String SHADERS_SOURCE = "texture-copy";
+    private final String SHADERS_ROOT = "src/data/gl_430";
+    private final String TEXTURE_DIFFUSE = "kueken7_rgba8_srgb.dds";
+
+    // With DDS textures, v texture coordinate are reversed, from top to bottom
+    private int vertexCount = 6;
+    private int vertexSize = vertexCount * 2 * Vec2.SIZEOF;
+    private float[] vertexData = {
+        -1.0f, -1.0f, 0.0f, 1.0f,
+        +1.0f, -1.0f, 1.0f, 1.0f,
+        +1.0f, +1.0f, 1.0f, 0.0f,
+        +1.0f, +1.0f, 1.0f, 0.0f,
+        -1.0f, +1.0f, 0.0f, 0.0f,
+        -1.0f, -1.0f, 0.0f, 1.0f};
+
+    private enum Texture {
+        DIFFUSE,
+        COPY,
+        MAX
+    };
+
+    private enum Buffer {
+        VERTEX,
+        TRANSFORM,
+        MAX
+    }
+
+    private int[] pipelineName = {0}, vertexArrayName = {0}, bufferName = new int[Buffer.MAX.ordinal()],
+            textureName = new int[Texture.MAX.ordinal()];
+    private int programName;
+
+    @Override
+    protected boolean begin(GL gl) {
+
+        GL4 gl4 = (GL4) gl;
+
+        boolean validated = true;
+        validated = validated && checkExtension(gl4, "GL_ARB_copy_image");
+
+        if (validated) {
+            validated = initTexture(gl4);
+        }
+        if (validated) {
+            validated = initProgram(gl4);
+        }
+        if (validated) {
+            validated = initBuffer(gl4);
+        }
+        if (validated) {
+            validated = initVertexArray(gl4);
+        }
+
+        return validated;
+    }
+
+    private boolean initProgram(GL4 gl4) {
+
+        boolean validated = true;
+
+        // Create program
+        if (validated) {
+
+            ShaderCode vertShaderCode = ShaderCode.create(gl4, GL_VERTEX_SHADER,
+                    this.getClass(), SHADERS_ROOT, null, SHADERS_SOURCE, "vert", null, true);
+            ShaderCode fragShaderCode = ShaderCode.create(gl4, GL_FRAGMENT_SHADER,
+                    this.getClass(), SHADERS_ROOT, null, SHADERS_SOURCE, "frag", null, true);
+
+            ShaderProgram shaderProgram = new ShaderProgram();
+            shaderProgram.init(gl4);
+
+            programName = shaderProgram.program();
+
+            gl4.glProgramParameteri(programName, GL_PROGRAM_SEPARABLE, GL_TRUE);
+
+            shaderProgram.add(vertShaderCode);
+            shaderProgram.add(fragShaderCode);
+
+            shaderProgram.link(gl4, System.out);
+        }
+
+        // Get variables locations
+        if (validated) {
+
+            gl4.glGenProgramPipelines(1, pipelineName, 0);
+            gl4.glUseProgramStages(pipelineName[0], GL_VERTEX_SHADER_BIT | GL_FRAGMENT_SHADER_BIT, programName);
+        }
+
+        return validated & checkError(gl4, "initProgram");
+    }
+
+    private boolean initBuffer(GL4 gl4) {
+
+        gl4.glGenBuffers(Buffer.MAX.ordinal(), bufferName, 0);
+
+        gl4.glBindBuffer(GL_ARRAY_BUFFER, bufferName[Buffer.VERTEX.ordinal()]);
+        FloatBuffer vertexBuffer = GLBuffers.newDirectFloatBuffer(vertexData);
+        gl4.glBufferData(GL_ARRAY_BUFFER, vertexSize, vertexBuffer, GL_STATIC_DRAW);
+        gl4.glBindBuffer(GL_ARRAY_BUFFER, 0);
+
+        gl4.glBindBuffer(GL_UNIFORM_BUFFER, bufferName[Buffer.TRANSFORM.ordinal()]);
+        gl4.glBufferData(GL_UNIFORM_BUFFER, Mat4.SIZEOF, null, GL_DYNAMIC_DRAW);
+        gl4.glBindBuffer(GL_UNIFORM_BUFFER, 0);
+
+        return true;
+    }
+
+    private boolean initTexture(GL4 gl4) {
+
+        boolean validated = true;
+
+        try {
+
+            gl4.glPixelStorei(GL_UNPACK_ALIGNMENT, 1);
+
+            gl4.glGenTextures(Texture.MAX.ordinal(), textureName, 0);
+
+            jgli.Texture2d texture = new Texture2d(jgli.Load.load(TEXTURE_ROOT + "/" + TEXTURE_DIFFUSE));
+            assert (!texture.empty());
+            jgli.Gl.Format format = jgli.Gl.translate(texture.format());
+
+            gl4.glActiveTexture(GL_TEXTURE0);
+            gl4.glBindTexture(GL_TEXTURE_2D, textureName[Texture.DIFFUSE.ordinal()]);
+            gl4.glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+            gl4.glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+            gl4.glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_BASE_LEVEL, 0);
+            gl4.glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAX_LEVEL, texture.levels() - 1);
+            gl4.glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_SWIZZLE_R, GL_RED);
+            gl4.glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_SWIZZLE_G, GL_GREEN);
+            gl4.glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_SWIZZLE_B, GL_BLUE);
+            gl4.glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_SWIZZLE_A, GL_ALPHA);
+
+            // Set image
+            for (int level = 0; level < texture.levels(); ++level) {
+
+                gl4.glTexImage2D(GL_TEXTURE_2D, level,
+                        format.internal.value,
+                        texture.dimensions(level)[0], texture.dimensions(level)[1],
+                        0,
+                        format.external.value, format.type.value,
+                        texture.data(level));
+            }
+
+            // Allocate texture storage of texture::COPY.
+            gl4.glBindTexture(GL_TEXTURE_2D, textureName[Texture.COPY.ordinal()]);
+            gl4.glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+            gl4.glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+            gl4.glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_BASE_LEVEL, 0);
+            gl4.glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAX_LEVEL, texture.levels() - 1);
+            gl4.glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_SWIZZLE_R, GL_RED);
+            gl4.glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_SWIZZLE_G, GL_GREEN);
+            gl4.glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_SWIZZLE_B, GL_BLUE);
+            gl4.glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_SWIZZLE_A, GL_ALPHA);
+
+            for (int level = 0; level < texture.levels(); ++level) {
+
+                gl4.glTexImage2D(GL_TEXTURE_2D, level,
+                        format.internal.value,
+                        texture.dimensions(level)[0], texture.dimensions(level)[1],
+                        0,
+                        format.external.value, format.type.value,
+                        null);
+            }
+
+            gl4.glBindTexture(GL_TEXTURE_2D, 0);
+
+            // Fill texture data of texture::COPY from texture::DIFFUSE.
+            for (int level = 0; level < texture.levels(); ++level) {
+
+                gl4.glCopyImageSubData(
+                        textureName[Texture.DIFFUSE.ordinal()], GL_TEXTURE_2D, level, 0, 0, 0,
+                        textureName[Texture.COPY.ordinal()], GL_TEXTURE_2D, level, 0, 0, 0,
+                        texture.dimensions(level)[0], texture.dimensions(level)[1], 1);
+            }
+
+            gl4.glPixelStorei(GL_UNPACK_ALIGNMENT, 4);
+
+        } catch (IOException ex) {
+            Logger.getLogger(Gl_430_texture_copy.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return validated;
+    }
+
+    private boolean initVertexArray(GL4 gl4) {
+
+        gl4.glGenVertexArrays(1, vertexArrayName, 0);
+        gl4.glBindVertexArray(vertexArrayName[0]);
+        {
+            gl4.glBindBuffer(GL_ARRAY_BUFFER, bufferName[Buffer.VERTEX.ordinal()]);
+            gl4.glVertexAttribPointer(Semantic.Attr.POSITION, 2, GL_FLOAT, false, 2 * Vec2.SIZEOF, 0);
+            gl4.glVertexAttribPointer(Semantic.Attr.TEXCOORD, 2, GL_FLOAT, false, 2 * Vec2.SIZEOF, Vec2.SIZEOF);
+            gl4.glBindBuffer(GL_ARRAY_BUFFER, 0);
+
+            gl4.glEnableVertexAttribArray(Semantic.Attr.POSITION);
+            gl4.glEnableVertexAttribArray(Semantic.Attr.TEXCOORD);
+        }
+        gl4.glBindVertexArray(0);
+
+        return true;
+    }
+
+    @Override
+    protected boolean render(GL gl) {
+
+        GL4 gl4 = (GL4) gl;
+
+        {
+            Mat4 model = new Mat4(1.0f);
+            Mat4 projection = glm.perspective_((float) Math.PI * 0.25f, (float) windowSize.x / windowSize.y, 0.1f, 100.0f);
+            Mat4 mvp = projection.mul(viewMat4()).mul(model);
+
+            gl4.glBindBuffer(GL_UNIFORM_BUFFER, bufferName[Buffer.TRANSFORM.ordinal()]);
+            ByteBuffer pointer = gl4.glMapBufferRange(GL_UNIFORM_BUFFER, 0, Mat4.SIZEOF,
+                    GL_MAP_WRITE_BIT | GL_MAP_INVALIDATE_BUFFER_BIT);
+
+            pointer.asFloatBuffer().put(mvp.toFA_());
+
+            gl4.glUnmapBuffer(GL_UNIFORM_BUFFER);
+        }
+
+        gl4.glViewportIndexedfv(0, new float[]{0, 0, windowSize.x, windowSize.y}, 0);
+        gl4.glClearBufferfv(GL_COLOR, 0, new float[]{1.0f, 0.5f, 0.0f, 1.0f}, 0);
+
+        gl4.glBindProgramPipeline(pipelineName[0]);
+        gl4.glActiveTexture(GL_TEXTURE0);
+        gl4.glBindTexture(GL_TEXTURE_2D, textureName[Texture.COPY.ordinal()]);
+        gl4.glBindBufferBase(GL_UNIFORM_BUFFER, Semantic.Uniform.TRANSFORM0, bufferName[Buffer.TRANSFORM.ordinal()]);
+        gl4.glBindVertexArray(vertexArrayName[0]);
+
+        gl4.glDrawArraysInstancedBaseInstance(GL_TRIANGLES, 0, vertexCount, 1, 0);
+
+        return true;
+    }
+
+    @Override
+    protected boolean end(GL gl) {
+
+        GL4 gl4 = (GL4) gl;
+
+        gl4.glDeleteProgramPipelines(1, pipelineName, 0);
+        gl4.glDeleteBuffers(Buffer.MAX.ordinal(), bufferName, 0);
+        gl4.glDeleteProgram(programName);
+        gl4.glDeleteTextures(Texture.MAX.ordinal(), textureName, 0);
+        gl4.glDeleteVertexArrays(1, vertexArrayName, 0);
+
+        return true;
+    }
+}
