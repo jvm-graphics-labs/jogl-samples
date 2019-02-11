49c733fecd2e32295b809ce66ba54d7f7385f97b
Giuseppe Barbieri <elect86@gmail.com>
2019-02-11 23:42:35 +0100

    up

--- src/main/kotlin/oglSamples/tmp.kt
+++ src/main/kotlin/oglSamples/tmp.kt
@@ -1,7 +1,16 @@
 package oglSamples
 
+import glm_.glm
+import org.lwjgl.opengl.GL15C
 import uno.glfw.glfw
+import java.nio.IntBuffer
 
 operator fun glfw.invoke(block: glfw.() -> Unit) {
     glfw.block()
 }
+
+infix fun Int.wo(b: Int) = and(b.inv())
+
+fun glBeginQuery(target: Int, id: IntBuffer) = GL15C.glBeginQuery(target, id[0])
+
+fun glGetQueryObjectui(id: IntBuffer, name: Int) = GL15C.glGetQueryObjectui(id[0], name)
